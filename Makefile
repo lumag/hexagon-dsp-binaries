d271d63264f790aba9517a759cc6aedb56b6b408
Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
2024-11-05 15:09:47 +0000

    Makefile: support creating release archives
    Add simple script to create release archive, containing only necessary
    sets of binary data.
    
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>

--- Makefile
+++ Makefile
@@ -7,3 +7,17 @@ all:
 
 install:
 	./scripts/install.sh config.txt ${DESTDIR}/${DSPDIR}
+
+TAG = $(shell git describe)
+NAME = hexagon-dsp-binaries
+TARGET = $(NAME)_$(TAG).tar.gz
+SUBDIR = $(NAME)-$(TAG)
+dist:
+	$-rm -rf release
+	@mkdir -p release dist
+	./scripts/dist.sh config.txt release/$(SUBDIR)
+	tar -czf dist/$(TARGET) -C release $(SUBDIR)
+	@echo "Created dist/$(TARGET)"
+	@rm -rf release
+
+.PHONY: dist
---
+++ scripts/dist.sh
@@ -0,0 +1,50 @@
+#! /bin/sh
+# SPDX-License-Identifier: MIT
+#
+# Install DSP shell and libraries
+
+set -e
+
+if [ "$#" -ne 2 -o "$1" = "-h" -o "$1" = "--help" ]
+then
+	echo "Usage: $0 config.txt <destdir>" >&2
+	exit 1
+fi
+
+CFG="$1"
+DST="$2"
+
+if ! [ -r "${CFG}" ]
+then
+	echo "config ${CFG} is unreadable" >&2
+	exit 1
+fi
+
+mkdir -p "${DST}"
+
+# dest subdir DSP QC_IMAGE_VERSION_STRING
+# FIXME: maybe install only a fixed set of files
+do_install() {
+	local srcdir="$2/$4"
+	local dstdir="$1/$2/$4"
+
+	mkdir -p "${dstdir}"
+	install -m 0644 "${srcdir}"/* "${dstdir}"
+}
+
+grep -v '^#\|^$' "${CFG}" | while read verb rest
+do
+	case ${verb} in
+	"Install:" )
+		do_install ${DST} ${rest}
+		;;
+	"Link:" )
+		# ignore
+		;;
+	"*" )
+		echo "Unsupported clause ${verb}" >&2
+		echo 
+	esac
+done
+
+cp -r scripts/ Makefile WHENCE config.txt ${DST}
